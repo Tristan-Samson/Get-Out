9e28c96a85208308367007baf89104b5c1367d3e
Alexis <Alexis.perront@hotmail.fr>
2020-06-25 14:39:19 +0200

    debug

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,22 +17,24 @@ class SecurityController extends AbstractController
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
-         if ($this->getUser()) {
-             return $this->redirectToRoute('dashboard_index');
-      
+        if ($this->getUser()) {
+            return $this->redirectToRoute('dashboard_index');
+        }
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
+            return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        }
+
+
+        /**
+         * @Route("/logout", name="app_logout")
+         */
+        public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
